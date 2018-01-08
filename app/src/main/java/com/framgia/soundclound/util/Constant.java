f301ebdcdb02ccacb3ea95fea1fb2e0fc0480f43
NamBD <bui.danh.nam@framgia.com>
2018-01-08 14:12:08 +0700

    create tab home

---
+++ app/src/main/java/com/framgia/soundclound/util/Constant.java
@@ -0,0 +1,10 @@
+package com.framgia.soundclound.util;
+
+/**
+ * Created by Bui Danh Nam on 8/1/2018.
+ */
+
+public class Constant {
+    public static final int NUM_COLUMN_GRID = 3;
+    public static final int NUM_ROW_GIRD = 3;
+}
