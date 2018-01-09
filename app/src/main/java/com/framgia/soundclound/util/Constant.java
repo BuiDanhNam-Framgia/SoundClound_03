802b2358c130bf7d72dbe7afc9b72e95c1ac5417
DESKTOP-LPVM6O0\Sony <nguyenduc0496@gmail.com>
2018-01-09 09:40:05 +0700

    get tracks

--- app/src/main/java/com/framgia/soundclound/util/Constant.java
+++ app/src/main/java/com/framgia/soundclound/util/Constant.java
@@ -7,4 +7,10 @@ package com.framgia.soundclound.util;
 public class Constant {
     public static final int NUM_COLUMN_GRID = 3;
     public static final int NUM_ROW_GIRD = 3;
+    public static final String BASE_URL = "https://api-v2.soundcloud.com/";
+    public static final String PARA = "charts?kind=top&genre=soundcloud%3Agenres%3A";
+    public static final String CLIENT_ID = "client_id";
+    public static final String OFF_SET = "offset";
+    public static final String LIMIT = "limit";
+    public static final String ERROR_NULL = "null_data";
 }
